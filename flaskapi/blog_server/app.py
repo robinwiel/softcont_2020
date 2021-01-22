48a25f67aaaf81b312461166e7ba2fb73aefe808
Robin <robinwiel>
2021-01-22 20:14:25 +0100

    Include CORS

--- flaskapi/blog_server/app.py
+++ flaskapi/blog_server/app.py
@@ -1,8 +1,10 @@
 import os
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+CORS(app)
 
 app.config.from_object(os.environ['APP_SETTINGS'])
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
