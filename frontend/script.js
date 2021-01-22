dceb9c9edb4df9dc29c9255e1cdd59fbdb628596
Robin <robinwiel@gmail.com>
2021-01-22 17:39:16 +0100

    Include GET and POST in frontend

--- frontend/script.js
+++ frontend/script.js
@@ -1,4 +1,4 @@
-const hostVar = "localhost:8000";
+const hostVar = "http://localhost:8000";
 const endpointHello = "/";
 const endpointPosts = "/posts";
 
@@ -36,6 +36,45 @@ $(document).ready(function(){
                 console.log("Request failed.");
             }
           }, "json");
+    });
+
+    $("#reset_posts_btn").click(function(){
+        $.ajax({
+            type: "DELETE",
+            url: hostVar + endpointPosts,
+            success: function(data) {
+                $("#table_blogposts_body").empty();
+            },
+            failure: function(data) {
+                console.log("Request failed.");
+            }
+          }, "json");
+    });
+
+    $("#post_form").submit(function (event) {
+        event.preventDefault();
+
+        var entry = {
+            author: $("input[name=author]").val(),
+            date: $("input[name=date]").val(),
+            text: $("textarea[name=text]").val()
+        };
+
+        $.ajax({
+            url: hostVar + endpointPosts,
+	        contentType: 'application/json',
+            data: JSON.stringify(entry),
+            type: 'POST',
+            success: function(data) {
+                $("#table_blogposts_body").prepend(
+                    "<tr>" +
+                    "<td>" + data.author + "</td>" +
+                    "<td>" + data.date + "</td>" +
+                    "<td>" + data.text + "</td>" +
+                    "</tr>"
+                );
+            }
+        });
     });
 
 
