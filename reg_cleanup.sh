0aa55bb62ef3a5068f2a5eab3fbb9c43a7602084
Robin <robinwiel>
2021-01-27 18:52:52 +0100

    Add cleanup for k8s registry

---
+++ reg_cleanup.sh
@@ -0,0 +1,7 @@
+# get all images that start with localhost:32000, output the results into image_ls file
+sudo microk8s ctr images ls name~='localhost:32000' | awk {'print $1'} > image_ls
+# loop over file, remove each image
+cat image_ls | while read line || [[ -n $line ]];
+do
+    microk8s ctr images rm $line
+done;
