9e492f316053f3140ea0cdeb36a965dc764383b5
Robin <robinwiel>
2021-01-31 18:24:59 +0100

    Bugfix

--- reg_cleanup.sh
+++ reg_cleanup.sh
@@ -1,5 +1,6 @@
 # get all images that start with localhost:32000, output the results into image_ls file
 sudo microk8s ctr images ls name~='localhost:32000' | awk {'print $1'} > image_ls
+
 # loop over file, remove each image
 cat image_ls | while read line || [[ -n $line ]];
 do
